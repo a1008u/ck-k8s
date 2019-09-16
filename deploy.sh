6a251a69cf02a0b08bfb7c53e99981136ae716c1
akira uemoto <empty.aauu0001@gmail.com>
2019-09-16 18:45:54 +0900

    docker hubへのpush作成

--- deploy.sh
+++ deploy.sh
@@ -2,15 +2,15 @@ docker build -t stephengrider/multi-client:latest -t stephengrider/multi-client:
 docker build -t stephengrider/multi-server:latest -t stephengrider/multi-server:$SHA -f ./server/Dockerfile ./server
 docker build -t stephengrider/multi-worker:latest -t stephengrider/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 
-docker push stephengrider/multi-client:latest
-docker push stephengrider/multi-server:latest
-docker push stephengrider/multi-worker:latest
+docker push a1008u/multi-client:latest
+docker push a1008u/multi-server:latest
+docker push a1008u/multi-worker:latest
 
-docker push stephengrider/multi-client:$SHA
-docker push stephengrider/multi-server:$SHA
-docker push stephengrider/multi-worker:$SHA
+docker push a1008u/multi-client:$SHA
+docker push a1008u/multi-server:$SHA
+docker push a1008u/multi-worker:$SHA
 
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=stephengrider/multi-server:$SHA
-kubectl set image deployments/client-deployment client=stephengrider/multi-client:$SHA
-kubectl set image deployments/worker-deployment worker=stephengrider/multi-worker:$SHA
+kubectl set image deployments/server-deployment server=a1008u/multi-server:$SHA
+kubectl set image deployments/client-deployment client=a1008u/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=a1008u/multi-worker:$SHA
